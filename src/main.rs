786a57d9dd63d99ac5540e538e286d09de008ef8
clemkeirua <clement@keiruaprod.fr>
2023-04-01 22:33:35 +0200

    added basic descriptive statistics

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+#[macro_use]
+extern crate statrs;
 use rand::Rng;
 use rayon::prelude::*;
 use clap::Parser;
+use statrs::statistics::*;
+// use plotters::prelude::*;
 
 fn buy_meals_until_all_toys(n: usize, iterlimit: usize) -> usize {
     let mut owned_toys = vec![false; n];
@@ -15,15 +19,44 @@ fn buy_meals_until_all_toys(n: usize, iterlimit: usize) -> usize {
     panic!("Expected simulation to finish in {} iterations.", iterlimit);
 }
 
-fn estimate_expectation(nb_toys: usize, nb_iterations: usize) -> f64 {
+fn estimate_expectation(nb_toys: usize, nb_iterations: usize) -> Vec<f64> {
     assert!(nb_iterations > 0);
-    let total: usize = (0..nb_iterations)
+    let mut observations: Vec<_> = (0..nb_iterations)
         .into_par_iter()
-        .map(|_| buy_meals_until_all_toys(nb_toys, 5000))
-        .sum();
-    total as f64 / nb_iterations as f64
+        .map(|_| buy_meals_until_all_toys(nb_toys, 5000) as f64)
+        .collect();
+    observations
 }
 
-fn main() {
-    println!("{}", estimate_expectation(94, 5000));
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Number of toys to find
+    #[arg(short, long, default_value_t = 1)]
+    nb_toys: usize,
+
+    /// Number of iterations the simulation will run
+    #[arg(short, long, default_value_t = 1)]
+    iterations: usize,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+    
+    let observations = estimate_expectation(args.nb_toys, args.iterations);
+    let mut data = Data::new(observations);
+
+    let mut deciles = [0.; 10];
+    for i in 1..=10 {
+        let q = i as f64 / 10.0;
+        let quantile = data.quantile(q);
+        deciles[i - 1] = quantile;
+    }
+
+
+    println!("mean: {:?}", data.mean());
+    println!("deciles: {:?}", deciles);
+    Ok(())
 }
